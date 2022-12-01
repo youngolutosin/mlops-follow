776a218e478bcfb0a82450c2f92baa2a9c8f85f3
youngolutosin <youngolutosin@gmail.com>
2022-12-01 14:16:35 +0200

    logging and config

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,10 +1,7 @@
 from api.app import create_app
-from api.config import DevelopmentConfig
 
 
-application = create_app(
-    config_object=DevelopmentConfig)
-
+application = create_app()
 
 if __name__ == '__main__':
     application.run()
